0caa550be0a93e5fde54658eeb965abe49d95621
Rouvin <rouvinerh@gmail.com>
2024-11-19 23:21:26 +0100

    this works somehow

--- interaction.py
+++ interaction.py
@@ -1,3 +1,4 @@
+import math
 import fastf1 as ff1
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,14 +8,14 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.path import Path
 from plotly.subplots import make_subplots
-import math
+from shapely.geometry import Point, Polygon, LineString
 
 ### Set constants here for code ###
 YEAR = 2019
 GRAND_PRIX = 'German Grand Prix'
 SESSION_TYPE = 'R' 
 DRIVER = 'VER'
-
+selected_lap_numbers = [29, 55, 32]
 
 ### Load session, race data and weather data ###
 race = ff1.get_session(YEAR, GRAND_PRIX, SESSION_TYPE)
@@ -30,80 +31,73 @@ df = pd.read_csv(track_data_url)
 raceline_url = 'https://github.com/TUMFTM/racetrack-database/raw/e59595d1f3573b30d1ded6a08984935b957688e0/racelines/Hockenheim.csv'
 raceline_data = pd.read_csv(raceline_url, comment='#', header=None)  # Load the raceline data
 
-### Set laps for drivers (Max's) ###
-try:
-    # Get laps for the driver
-    driver_laps = race.laps.pick_driver(DRIVER)
+# Get laps for the driver
+driver_laps = race.laps.pick_driver(DRIVER)
+selected_laps = driver_laps.pick_laps(selected_lap_numbers)
 
-    # Check if there are any laps available for this driver
-    if driver_laps.empty:
-        print(f"No laps available for driver {DRIVER}")
+# Positions
+position_data_100 = []  # List to store position data for each lap at 100Hz
+position_data_orig = [] # List to store position data for each lap at original frequency
 
-    # Extract telemetry for the fastest lap of this driver
-    selected_laps = driver_laps.pick_laps(1)
-    #selected_laps = driver_laps#.pick_fastest() **************************** ROUVIN HERE SELECT THE LAP(S) YOU WANT
-    # **** IM JUST GONNA GO WITH THIS FIRST - Rouvin
-    # OR selected_laps = driver_laps.pick_laps(range(10,21))
-    # OR selected_laps = driver_laps.pick_laps(1)
+for lap_number in selected_lap_numbers:
+    lap_data = selected_laps[selected_laps['LapNumber'] == lap_number] # Filter data for current lap
 
-    # Get telemetry data at the highest available resolution (100 frequency) and the original one
-    telemetry = selected_laps.get_telemetry(frequency=100)  # Use 'original' for highest granularity
-    telemetry_original= selected_laps.get_telemetry(frequency='original')  # Use 'original' for highest granularity
+    # Get telemetry data at both frequencies
+    telemetry_100 = lap_data.get_telemetry(frequency=100)
+    telemetry_original = lap_data.get_telemetry(frequency='original')
 
-    # Check if positional telemetry is available (X, Y data)
-    if 'X' in telemetry.columns and 'Y' in telemetry.columns:
-        # Extract the x and y coordinates
-        x_coords = telemetry['X']/10 # Adjust from 1/10m -> 1m scale
-        y_coords = telemetry['Y']/10 # Adjust from 1/10m -> 1m scale
+    # Combine them into a DataFrame
+    position_data_100.append(pd.DataFrame({
+        'X': telemetry_100['X']/10, # Adjust from 1/10m -> 1m scale
+        'Y': telemetry_100['Y']/10, # Adjust from 1/10m -> 1m scale
+        'Lap': lap_number,
+        'Distance': telemetry_100['Distance'],
+        'Time': telemetry_100['Date']  # Add timestamps to check the data frequency
+    }))
+
+    # Combine them into a DataFrame
+    position_data_orig.append(pd.DataFrame({
+        'X': telemetry_original['X']/10, # Adjust from 1/10m -> 1m scale
+        'Y': telemetry_original['Y']/10, # Adjust from 1/10m -> 1m scale
+        'Lap': lap_number,
+        'Distance': telemetry_original['Distance'],
+        'Time': telemetry_original['Date']  # Add timestamps to check the data frequency
+    }))
+
+# Check if positional telemetry is available (X, Y data)
+if 'X' in telemetry_original.columns and 'Y' in telemetry_original.columns:
+    # Extract the x and y coordinates
+    x_coords = telemetry_original['X']/10 # Adjust from 1/10m -> 1m scale
+    y_coords = telemetry_original['Y']/10 # Adjust from 1/10m -> 1m scale
 
 
-    # Check if positional telemetry is available (X, Y data)
-    if 'X' in telemetry_original.columns and 'Y' in telemetry_original.columns:
-        # Extract the x and y coordinates
-        x_coords_original = telemetry_original['X']/10 # Adjust from 1/10m -> 1m scale
-        y_coords_original = telemetry_original['Y']/10 # Adjust from 1/10m -> 1m scale
+# Check if positional telemetry is available (X, Y data)
+if 'X' in telemetry_original.columns and 'Y' in telemetry_original.columns:
+    # Extract the x and y coordinates
+    x_coords_original = telemetry_original['X']/10 # Adjust from 1/10m -> 1m scale
+    y_coords_original = telemetry_original['Y']/10 # Adjust from 1/10m -> 1m scale
+
+    # Combine them into a DataFrame
+    position_data = pd.DataFrame({
+        'X': x_coords,
+        'Y': y_coords,
+        'Time': telemetry_original['Date']  # Add timestamps to check the data frequency
+    })
 
         # Combine them into a DataFrame
-        position_data = pd.DataFrame({
-            'X': x_coords,
-            'Y': y_coords,
-            'Time': telemetry['Date']  # Add timestamps to check the data frequency
-        })
+    position_data_original = pd.DataFrame({
+        'X': x_coords_original,
+        'Y': y_coords_original,
+        'Time': telemetry_original['Date']  # Add timestamps to check the data frequency
+    })
 
-            # Combine them into a DataFrame
-        position_data_original = pd.DataFrame({
-            'X': x_coords_original,
-            'Y': y_coords_original,
-            'Time': telemetry_original['Date']  # Add timestamps to check the data frequency
-        })
+    # Calculate the time difference between consecutive telemetry points
+    time_diff = position_data['Time'].diff().dt.total_seconds()
 
-        # Calculate the time difference between consecutive telemetry points
-        time_diff = position_data['Time'].diff().dt.total_seconds()
+    # Calculate the time difference between consecutive telemetry points
+    time_diff_original = position_data_original['Time'].diff().dt.total_seconds()
 
 
-        # Calculate the time difference between consecutive telemetry points
-        time_diff_original = position_data_original['Time'].diff().dt.total_seconds()
-
-        # # Print statistics about the frequency
-        # print(f"Telemetry for driver {DRIVER} at 100 frequency:")
-        # print(f"Number of data points: {len(position_data)}")
-        # print(f"Average time interval between points: {time_diff.mean()} seconds")
-        # print(position_data.head())  # Display first few rows of X, Y, and Time data
-
-        #     # Print statistics about the frequency
-        # print(f"Telemetry  for driver {DRIVER} at original frequency:")
-        # print(f"Number of data points: {len(position_data_original)}")
-        # print(f"Average time interval between points: {time_diff_original.mean()} seconds")
-        # print(position_data_original.head())  # Display first few rows of X, Y, and Time data
-        # # Exit loop after finding first driver with valid data
-
-    else:
-        print(f"No positional data (X, Y) available for driver {DRIVER}")
-
-except KeyError as e:
-    print(f"Telemetry not available for driver {DRIVER}: {e}")
-except Exception as e:
-    print(f"Error encountered for driver {DRIVER}: {e}")
 
 ### Extract the centerline and track width data (Max's) ###
 x = df['# x_m'].values  # Adjust based on the actual column name
@@ -141,17 +135,6 @@ y_right = np.append(y_right, y_right[0])
 # Ensure the left boundary is closed
 x_left = np.append(x_left, x_left[0])
 y_left = np.append(y_left, y_left[0])
-
-# Plot the track
-# plt.plot(x, y, label='Centerline', color='black')
-# plt.plot(x_left, y_left, label='Left Boundary', color='blue')
-# plt.plot(x_right, y_right, label='Right Boundary', color='red')
-
-# plt.title("Hockenheim Track Outline")
-# plt.xlabel("X [m]")
-# plt.ylabel("Y [m]")
-# plt.legend()
-# plt.show()
 
 ### Define parameters that are interesting (Mad's) ###
 sector_times_df = laps[['Driver', 'LapNumber', 'Sector1Time', 'Sector2Time', 'Sector3Time', 'LapTime', 'Compound']].copy()
@@ -231,11 +214,10 @@ track_status_hierarchy = {
 }
 
 lap_events['TrackStatusHierarchy'] = lap_events['TrackStatus'].apply(get_worst_status)
-
 lap_events = lap_events.reset_index(drop=True)
 lap_events.index += 1
 lap_events = lap_events.iloc[:-1]
-print(lap_events)
+
 # Create dummy columns to indicate the fastest times for each sector and lap time
 sector_times_df['FastestSector1'] = (sector_times_df['Sector1Time'] == sector_times_df.groupby('Driver')['Sector1Time'].transform('min')).astype(int)
 sector_times_df['FastestSector2'] = (sector_times_df['Sector2Time'] == sector_times_df.groupby('Driver')['Sector2Time'].transform('min')).astype(int)
@@ -378,11 +360,11 @@ def create_combined_plot():
     offset_y = 198
 
     # Add the telemetry data as a line plot
-    fig.add_trace(go.Scatter(x=x_coords, y=y_coords, mode='lines',name=' data f=100',showlegend = False), row = 1, col = 2)
-    fig.add_trace(go.Scatter(x=x_coords_original, y=y_coords_original, mode='markers',name='original data',showlegend = False), row = 1, col = 2)
-    fig.add_trace(go.Scatter(x=x_left-offset_x, y=y_left-offset_y, mode='lines',name='right boundary',showlegend = False), row = 1, col = 2)
-    fig.add_trace(go.Scatter(x=x_right-offset_x, y=y_right-offset_y, mode='lines',name='left boundary',showlegend = False), row = 1, col = 2)
-    fig.add_trace(go.Scatter(x=raceline_data[0]-offset_x, y=raceline_data[1]-offset_y, mode='lines',name='Ideal racing line',showlegend = False, ), row = 1, col = 2)
+    # fig.add_trace(go.Scatter(x=x_coords, y=y_coords, mode='lines',name=' data f=100',showlegend = False), row = 1, col = 2)
+    # fig.add_trace(go.Scatter(x=x_coords_original, y=y_coords_original, mode='markers',name='original data',showlegend = False), row = 1, col = 2)
+    # fig.add_trace(go.Scatter(x=x_left-offset_x, y=y_left-offset_y, mode='lines',name='right boundary',showlegend = False), row = 1, col = 2)
+    # fig.add_trace(go.Scatter(x=x_right-offset_x, y=y_right-offset_y, mode='lines',name='left boundary',showlegend = False), row = 1, col = 2)
+    # fig.add_trace(go.Scatter(x=raceline_data[0]-offset_x, y=raceline_data[1]-offset_y, mode='lines',name='Ideal racing line',showlegend = False, ), row = 1, col = 2)
 
     x_left_shifted = x_left - offset_x
     y_left_shifted = y_left - offset_y
@@ -400,6 +382,36 @@ def create_combined_plot():
     # Create Path objects representing the right and left boundary polygons
     right_boundary_path = Path(right_boundary_points)
     left_boundary_path = Path(left_boundary_points)
+
+    inner_boundary = Polygon(zip(x_left_shifted, y_left_shifted))
+    outer_boundary = Polygon(zip(x_right_shifted, y_right_shifted))
+
+    for i in range(len(position_data_100)):
+        # Create a mask for valid points (Check if points are within outer but not inner boundary)
+        mask_100 = position_data_100[i].apply(lambda row: inner_boundary.contains(Point(row['X'], row['Y'])) and not outer_boundary.contains(Point(row['X'], row['Y'])), axis=1)
+        mask_orig = position_data_orig[i].apply(lambda row: inner_boundary.contains(Point(row['X'], row['Y'])) and not outer_boundary.contains(Point(row['X'], row['Y'])), axis=1)
+
+        # Update the DataFrames with filtered data
+        position_data_100[i] = position_data_100[i][mask_100]
+        position_data_orig[i] = position_data_orig[i][mask_orig]
+
+    for i, lap_number in enumerate(selected_lap_numbers):
+        fig.add_trace(go.Scatter(
+            x=position_data_100[i]['X'],
+            y=position_data_100[i]['Y'],
+            mode='lines',
+            name=f'Data f=100 Lap {lap_number}'
+        ), row = 1, col = 2)
+        fig.add_trace(go.Scatter(
+            x=position_data_orig[i]['X'],
+            y=position_data_orig[i]['Y'],
+            mode='markers',
+            name=f'Original Data Lap {lap_number}'
+        ), row = 1, col = 2)
+
+    fig.add_trace(go.Scatter(x=x_left - offset_x, y=y_left - offset_y, mode='lines', name='Right Boundary'), row = 1, col = 2)
+    fig.add_trace(go.Scatter(x=x_right - offset_x, y=y_right - offset_y, mode='lines', name='Left Boundary'), row = 1, col = 2)
+    fig.add_trace(go.Scatter(x=raceline_data[0] - offset_x, y=raceline_data[1] - offset_y, mode='lines', name='Ideal racing line'), row = 1, col = 2)
 
     # Arrays to store valid points
     valid_x_original = []
@@ -459,7 +471,112 @@ def create_combined_plot():
         )
     
     ### Plot 4: Line Graph ###
-    ### To be implemented ###
+    for i, lap_number in enumerate(selected_lap_numbers):
+    # Get valid x and y coordinates for the current lap
+        valid_x = position_data_100[i]['X'].values
+        valid_y = position_data_100[i]['Y'].values
+
+        # Calculate direction vectors (tangent vectors)
+        dx_race = np.diff(valid_x)
+        dy_race = np.diff(valid_y)
+
+        # Calculate magnitude of tangent vectors
+        tangent_norm_race = np.sqrt(dx_race**2 + dy_race**2)
+
+        # Normalize tangent vectors to get unit vectors
+        tangent_x_race = dx_race / tangent_norm_race
+        tangent_y_race = dy_race / tangent_norm_race
+
+        # Calculate normal vectors (perpendicular to tangent)
+        normal_x_race = -tangent_y_race
+        normal_y_race = tangent_x_race
+
+        # Calculate angle changes
+        dx_race = np.diff(normal_x_race)
+        dy_race = np.diff(normal_y_race)
+        change = np.sqrt(dx_race**2 + dy_race**2)
+
+        # Get distance values for the current lap
+        distances = position_data_100[i]['Distance'].values
+
+    left_boundary = LineString(zip(x_left_shifted, y_left_shifted))
+    right_boundary = LineString(zip(x_right_shifted, y_right_shifted))
+
+    relative_positions_all_laps = []
+
+    for i, lap_number in enumerate(selected_lap_numbers):
+
+        relative_positions = []
+        for x, y in zip(position_data_100[i]['X'].values, position_data_100[i]['Y'].values):
+            point = Point(x, y)
+
+            # Calculate distances to boundaries
+            distance_to_inner = point.distance(left_boundary)
+            distance_to_outer = point.distance(right_boundary)
+
+            # Calculate relative position
+            total_width = distance_to_inner + distance_to_outer
+            relative_position = distance_to_inner / total_width
+
+            relative_positions.append(relative_position)
+
+        relative_positions_all_laps.append(relative_positions)
+    for i, lap_number in enumerate(selected_lap_numbers):
+    # Access distances for the current lap
+        distances = position_data_100[i]['Distance'].values
+
+        # Access relative_positions for the current lap
+        # (Assuming you have a list of relative_positions for each lap)
+        # Example: relative_positions_all_laps = [relative_positions_lap1, relative_positions_lap2, ...]
+        relative_positions = relative_positions_all_laps[i]
+
+        fig.add_trace(go.Scatter(
+            x=distances,
+            y=relative_positions,
+            mode='lines',
+            name=f'Lap {lap_number}'
+        ), row = 2, col = 2)
+    max_distance = max(distances)  # Get the maximum distance value
+
+
+    # Add horizontal lines for boundaries (modified)
+    fig.add_shape(go.layout.Shape(
+        type="line",
+        x0=0,
+        x1=max_distance,  # Adjust x1 to cover the desired x-axis range
+        y0=0,
+        y1=0,
+        line=dict(color="Black", width=3, dash="solid"),  # Changed color, width, and dash
+    ), row = 2, col = 2)
+    fig.add_shape(go.layout.Shape(
+        type="line",
+        x0=0,
+        x1=max_distance,  # Adjust x1 to cover the desired x-axis range
+        y0=1,
+        y1=1,
+        line=dict(color="Black", width=3, dash="solid"),  # Changed color, width, and dash
+    ), row = 2, col = 2)
+
+
+    # Add annotations for boundary labels (modified)
+    fig.add_annotation(
+        x=max_distance * 0.15, # Adjust the position to be inside the plot
+        y=0.05,  # Position slightly below the line (adjust as needed)
+        text="Right Boundary",
+        showarrow=False,
+        font=dict(size=12),
+        xanchor="right",  # Anchor to the right to avoid overlapping with the line
+        yanchor="top", row = 2, col = 2   # Anchor to the top to position it below the line
+    )
+    fig.add_annotation(
+        x=max_distance * 0.13, # Adjust the position to be inside the plot
+        y=0.95,  # Position slightly above the line (adjust as needed)
+        text="Left Boundary",
+        showarrow=False,
+        font=dict(size=12),
+        xanchor="right",  # Anchor to the right to avoid overlapping with the line
+        yanchor="bottom", row = 2, col = 2  # Anchor to the bottom to position it above the line
+    )
 
     ### Update Layout of the Graphs ###
     fig.update_layout(
@@ -502,8 +619,9 @@ def create_combined_plot():
     fig.update_yaxes(title_text = "Lap Times (s)", range = [0, sector_times_df['LapTime'].max() * 1], row = 2, col = 1)
 
     ## Plot 4
-    
-
+    fig.update_xaxes(title_text = "Distance (m)", row = 2, col = 2)
+    fig.update_yaxes(title_text = "Relative Position (0 = Outer Boundary, 1 = Inner Boundary)", row = 2, col = 2)
+ 
     fig.show()
 
 create_combined_plot()
